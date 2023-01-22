9828e0bd8519e231b9e31a374c6042b46d5862bc
Adrian Gamboa Delgado <adriangd1800@gmail.com>
2023-01-22 15:10:50 -0600

    Ajustes generales

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -38,12 +38,12 @@ class AuthController extends Controller
     public function login(Request $request){
         if(!Auth::attempt($request->only('email','password')))
         {
-            return response()->json(['message' => 'Unauthorized'], 401);
+            return response()->json(['message' => 'Unauthorized']);
         }
 
         $user = User::where('email', $request['email'])->firstOrFail();
         $token = $user->createToken('auth_token')->plainTextToken;
-        return response()->json(['message' => 'Hi '.$user->name,'access_token' => $token, 'token_type' => 'Bearer','user'=>$user,]);
+        return response()->json(['message' => 'Authorized','access_token' => $token, 'token_type' => 'Bearer','user'=>$user,]);
     }
 
     public function logout(Request $request) {
